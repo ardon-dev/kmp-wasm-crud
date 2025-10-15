b5c6cbf8edb53bc01bdd8e842a9602b8bca30081
Ardón <josueardon.developer@gmail.com>
2025-10-14 23:33:14 -0600

    feat(contacts): custom text field component

---
+++ composeApp/src/webMain/kotlin/com/ardondev/contactsapp/core/components/CustomTextField.kt
@@ -0,0 +1,53 @@
+package com.ardondev.contactsapp.core.components
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+
+@Composable
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    singleLine: Boolean = false,
+    label: String? = null,
+    leadingIcon: ImageVector? = null,
+    isError: Boolean = false,
+    supportingText: String? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType
+        ),
+        singleLine = singleLine,
+        label = if (label != null) {
+            { Text(label) }
+        } else null,
+        leadingIcon = if (leadingIcon != null) {
+            {
+                Icon(
+                    imageVector = leadingIcon,
+                    contentDescription = null
+                )
+            }
+        } else null,
+        isError = isError,
+        supportingText = if (supportingText != null) {
+            {
+                Text(supportingText)
+            }
+        } else null,
+        visualTransformation = visualTransformation,
+        modifier = modifier
+    )
+}
