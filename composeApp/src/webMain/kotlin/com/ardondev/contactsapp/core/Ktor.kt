6b81ba92751810fa4649f95f34a62b149b651b25
Ardón <josueardon.developer@gmail.com>
2025-10-14 23:32:56 -0600

    feat(contacts): added Ktor Http Client

---
+++ composeApp/src/webMain/kotlin/com/ardondev/contactsapp/core/Ktor.kt
@@ -0,0 +1,36 @@
+package com.ardondev.contactsapp.core
+
+import io.ktor.client.*
+import io.ktor.client.engine.js.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+object KtorClient {
+
+    private val _client = HttpClient(Js) {
+        install(Logging)
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    val client: HttpClient
+        get() = _client
+
+    @Serializable
+    data class Error(
+        @SerialName("code"      ) var code      : Int?    = null,
+        @SerialName("error_code") var errorCode : String? = null,
+        @SerialName("msg"       ) var msg       : String? = null,
+    )
+
+}
+
