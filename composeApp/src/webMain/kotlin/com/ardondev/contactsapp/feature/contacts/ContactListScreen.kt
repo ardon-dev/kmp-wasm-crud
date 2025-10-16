0625538d210521487806b6875da0be712f2e388b
Ardón <josueardon.developer@gmail.com>
2025-10-16 09:50:33 -0600

    refactor(contacts): added uiState

--- composeApp/src/webMain/kotlin/com/ardondev/contactsapp/feature/contacts/ContactListScreen.kt
+++ composeApp/src/webMain/kotlin/com/ardondev/contactsapp/feature/contacts/ContactListScreen.kt
@@ -1,16 +1,19 @@
 package com.ardondev.contactsapp.feature.contacts
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.serialization.Serializable
 
 @Serializable
-object ContactsRoute
-
-data class ContactListUiState(
-    val contacts: List<Any> = emptyList(),
-)
+object ContactListScreenRoute
 
 @Composable
-fun ContactListScreen() {
+fun ContactListScreen(
+    viewModel: ContactListViewModel = viewModel { ContactListViewModel() }
+) {
+
+    val uiState by viewModel.uiState.collectAsState()
 
 }
--- composeApp/src/webMain/kotlin/com/ardondev/contactsapp/feature/contacts/ContactListViewModel.kt
+++ composeApp/src/webMain/kotlin/com/ardondev/contactsapp/feature/contacts/ContactListViewModel.kt
@@ -1,4 +1,13 @@
 package com.ardondev.contactsapp.feature.contacts
 
-class ContactListViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ContactListViewModel: ViewModel() {
+
+    private val _uiState = MutableStateFlow(ContactListUiState())
+    val uiState: StateFlow<ContactListUiState> = _uiState.asStateFlow()
+
 }
