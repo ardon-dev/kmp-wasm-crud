0c57c18a5b99d5185901f97d6f4531a624e55f28
Ardón <josueardon.developer@gmail.com>
2025-10-16 14:52:41 -0600

    feat(ui): application theme

--- composeApp/src/webMain/kotlin/com/ardondev/contactsapp/App.kt
+++ composeApp/src/webMain/kotlin/com/ardondev/contactsapp/App.kt
@@ -1,8 +1,8 @@
 package com.ardondev.contactsapp
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import com.ardondev.contactsapp.core.Session
+import com.ardondev.contactsapp.core.theme.AppTheme
 import com.ardondev.contactsapp.feature.login.LoginScreen
 import com.ardondev.contactsapp.feature.main.MainScreen
 import kotlinx.browser.window
@@ -13,7 +13,7 @@ fun App() {
     val isAuthenticated = Session.fetchValue(Session.KEY_TOKEN) != null
     println("Authenticated user: $isAuthenticated")
 
-    MaterialTheme {
+    AppTheme {
         if (isAuthenticated) {
             MainScreen()
         } else {
---
+++ composeApp/src/webMain/kotlin/com/ardondev/contactsapp/core/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.ardondev.contactsapp.core.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
