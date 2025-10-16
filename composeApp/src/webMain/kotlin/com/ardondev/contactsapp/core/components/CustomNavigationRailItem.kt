1971fa2c8c4f4e53529cc66457a27248c6b94d1c
Ardón <josueardon.developer@gmail.com>
2025-10-16 14:55:22 -0600

    refactor(core): custom navigation rail item colors

--- composeApp/src/webMain/kotlin/com/ardondev/contactsapp/core/components/CustomNavigationRailItem.kt
+++ composeApp/src/webMain/kotlin/com/ardondev/contactsapp/core/components/CustomNavigationRailItem.kt
@@ -1,7 +1,9 @@
 package com.ardondev.contactsapp.core.components
 
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.NavigationRailItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,6 +31,12 @@ fun CustomNavigationRailItem(
                 contentDescription = null
             )
         },
+        colors = NavigationRailItemDefaults.colors(
+            selectedIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            unselectedIconColor = MaterialTheme.colorScheme.onPrimary,
+            selectedTextColor = MaterialTheme.colorScheme.onPrimary,
+            unselectedTextColor = MaterialTheme.colorScheme.onPrimary
+        ),
         modifier = modifier
     )
 
