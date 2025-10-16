2bccb1255f88fa2ca943ee096e6762c465c0bd54
Ardón <josueardon.developer@gmail.com>
2025-10-15 20:25:07 -0600

    refactor(ui): custom text field

--- composeApp/src/webMain/kotlin/com/ardondev/contactsapp/core/components/CustomTextField.kt
+++ composeApp/src/webMain/kotlin/com/ardondev/contactsapp/core/components/CustomTextField.kt
@@ -2,8 +2,8 @@ package com.ardondev.contactsapp.core.components
 
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -23,7 +23,7 @@ fun CustomTextField(
     supportingText: String? = null,
     visualTransformation: VisualTransformation = VisualTransformation.None
 ) {
-    OutlinedTextField(
+    TextField(
         value = value,
         onValueChange = onValueChange,
         keyboardOptions = KeyboardOptions(
